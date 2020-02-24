b71e82dced0bcea61119170255282f4020386fd2
redmaze <emailpribadiilyas@gmail.com>
2020-02-24 15:02:08 +0700

    change structure sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './stores';
-import './index.scss';
+import './assets/scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
