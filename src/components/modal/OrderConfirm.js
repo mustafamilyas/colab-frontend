7420c322eecb2390d7482a513e63407ec660b86f
redmaze <emailpribadiilyas@gmail.com>
2020-02-26 09:59:56 +0700

    add styling to modal order confirm

--- src/components/modal/OrderConfirm.js
+++ src/components/modal/OrderConfirm.js
@@ -5,8 +5,31 @@ class OrderConfirm extends Component {
     render() { 
         return (
             <Modal>
-                <div>
-                    Order Confirm
+                <div className='order-confirm'>
+                    <div className='order-confirm__title'>
+                        Order Summary
+                    </div>
+                    <div className='order-confirm__consument'>
+                        <div>
+                            <div className='bold'>Your Name</div>
+                            <div>Ilyas</div>
+                        </div>
+                        <div>
+                            <div className='bold'>Table Number</div>
+                            <div>C12</div>
+                        </div>
+                    </div>
+                    <div className='order-confirm__summary'>
+                        &nbsp;
+                    </div>
+                    <div className='order-confirm__btn-group'>
+                        <button className='order-confirm__btn order-confirm__btn--invert'>
+                            Cancel
+                        </button>
+                        <button className='order-confirm__btn order-confirm__btn--primary'>
+                            Submit My Order
+                        </button>
+                    </div>
                 </div>
             </Modal>
         );
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -7,7 +7,7 @@ import OrderSuccess from '../components/Modal/OrderSuccess';
 
 class Order extends Component {
     state = {
-        phase: 0
+        phase: 1
     }
     render() { 
         const modal = [
